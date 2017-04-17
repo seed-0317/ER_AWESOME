b927910ab015c8f6866aee9903612aa63937a05e
Morris <qzh225@784f4385bef8.cof.ds.capitalone.com>
2017-04-17 17:15:04 -0400

    Added DAO files and methods, but nethods aren't coded yet

---
+++ ers/src/main/java/dao/UserDaoImpl.java
@@ -0,0 +1,27 @@
+package dao;
+
+/**
+ * Created by qzh225 on 4/17/17.
+ */
+
+import model.User;
+import java.util.ArrayList;
+
+public class UserDaoImpl implements UserDao {
+
+    public ArrayList<User> getAllUsers(){
+
+        return null;
+    }
+
+    public void updateEmployee(User emp){
+
+    }
+
+    public User getUser (int userId){
+
+        return null;
+    }
+
+
+}
