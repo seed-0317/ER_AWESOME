0fb926fa5e5d72e4687399551a5d347deb8b99ad
Stephen Phillips <Sbphillips19@gmail.com>
2017-04-18 16:24:56 -0400

    adding user update html and modify servlet

--- ers/src/main/java/servlet/UpdatePersonalDataServlet.java
+++ ers/src/main/java/servlet/UpdatePersonalDataServlet.java
@@ -15,7 +15,7 @@ public class UpdatePersonalDataServlet extends HttpServlet{
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        req.getRequestDispatcher("empInfo").forward(req,resp);
     }
 
     @Override
